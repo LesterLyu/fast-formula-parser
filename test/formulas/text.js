3c8b07271f986e4c260540d61e86888bf0639ce4
Lester Lyu <lester.lyu@ontario.ca>
2019-03-28 11:04:14 -0400

    Add test skeleton

---
+++ test/formulas/text.js
@@ -0,0 +1,10 @@
+const chai = require('chai');
+const expect = require('chai').expect;
+const TextFunctions = require('../../formulas/text');
+
+describe('Text Functions', function () {
+    it('ASC should ..', function () {
+        const result = TextFunctions.ASC();
+        expect(result).to.equal(undefined)
+    });
+});
