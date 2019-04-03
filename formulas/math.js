3806dc4f32bcd0a3838805256fb84a8396f01169
LesterLyu <lvds2000@gmail.com>
2019-04-03 00:11:27 -0400

    Implement more math functions.

--- formulas/math.js
+++ formulas/math.js
@@ -1,6 +1,16 @@
 const FormulaError = require('./error');
-const {FormulaHelpers, Types} = require('./helpers');
+const {FormulaHelpers, Types, Factorials} = require('./helpers');
 const H = FormulaHelpers;
+
+// factorials
+const f = [];
+function factorial(n) {
+    if (n <= 100)
+        return Factorials[n];
+    if (f[n] > 0)
+        return f[n];
+    return f[n] = factorial(n-1) * n;
+}
 
 // https://support.office.com/en-us/article/excel-functions-by-category-5f91f4e9-7b42-46d2-9bd1-63f26a86c0eb
 const MathFunctions = {
@@ -57,16 +67,90 @@ const MathFunctions = {
         return new Array(Math.max(minLength + 1 - result.length, 0)).join('0') + result;
     },
 
-    'CEILING.MATH': (number, significance, mode = 0) => {
+    CEILING: (number, significance) => {
+        number = H.accept(number, [Types.NUMBER]);
+        if (number >= 9.99E+307 || number <= -2.229E-308)
+            throw FormulaError.NUM;
+        significance = H.accept(significance, [Types.NUMBER]);
+        if (significance === 0)
+            return 0;
+        if (number / significance % 1 === 0)
+            return number;
+        const absSignificance = Math.abs(significance);
+        const times = Math.floor(Math.abs(number) / absSignificance);
+        if (number < 0) {
+            // round down, away from zero
+            const roundDown = significance < 0;
+            return roundDown ? -absSignificance * (times + 1) : -absSignificance * (times);
+        } else {
+            return (times + 1) * absSignificance;
+        }
+    },
+
+    'CEILING.MATH': (number, significance, mode) => {
         number = H.accept(number, [Types.NUMBER]);
         if (number >= 9.99E+307 || number <= -2.229E-308)
             throw FormulaError.NUM;
         significance = H.accept(significance, [Types.NUMBER], true);
         if (significance === undefined)
             significance = number > 0 ? 1 : -1;
-        mode = H.accept(mode, [Types.NUMBER]);
+        // mode can be 0 or -1 or undefined
+        mode = H.accept(mode, [Types.NUMBER], true);
+        // The Mode argument does not affect positive numbers.
+        if (mode === undefined || number > 0) {
+            return MathFunctions.CEILING(number, significance);
+        }
+        // if round down, away from zero, then significance
+        const offset = mode ? significance : 0;
+        return MathFunctions.CEILING(number, significance) - offset;
+    },
 
+    'CEILING.PRECISE': (number, significance) => {
+        number = H.accept(number, [Types.NUMBER]);
+        significance = H.accept(significance, [Types.NUMBER], true);
+        if (significance === undefined) significance = 1;
+        // always round up
+        return MathFunctions.CEILING(number, Math.abs(significance));
+    },
 
+    COMBIN: (number, numberChosen) => {
+        number = H.accept(number, [Types.NUMBER]);
+        numberChosen = H.accept(numberChosen, [Types.NUMBER]);
+        if (number < 0 || numberChosen < 0 || number < numberChosen)
+            throw FormulaError.NUM;
+        const nFactorial = MathFunctions.FACT(number), kFactorial = MathFunctions.FACT(numberChosen);
+        return nFactorial / kFactorial / MathFunctions.FACT(number - numberChosen);
+    },
+
+    COMBINA: (number, numberChosen) => {
+        number = H.accept(number, [Types.NUMBER]);
+        numberChosen = H.accept(numberChosen, [Types.NUMBER]);
+        // special case
+        if ((number === 0 || number === 1) && numberChosen === 0)
+            return 1;
+        if (number < 0 || numberChosen < 0)
+            throw FormulaError.NUM;
+        return MathFunctions.COMBIN(number + numberChosen - 1, number - 1);
+    },
+
+    DECIMAL: (text, radix) => {
+        text = H.accept(text, [Types.STRING]);
+        radix = H.accept(radix, [Types.NUMBER]);
+        radix = Math.trunc(radix);
+        if (radix < 2 || radix > 36)
+            throw FormulaError.NUM;
+        return parseInt(text, radix);
+    },
+
+    FACT: (number) => {
+        number = H.accept(number, [Types.NUMBER]);
+        // max number = 170
+        if (number > 170 || number < 0)
+            throw FormulaError.NUM;
+        if (number <= 100)
+            return Factorials[number];
+        number = Math.trunc(number);
+        return factorial(number);
     },
 
     ROUND: (number, digits) => {
