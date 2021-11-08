4a7b9a11255e00fb166b35da8a9acc268d9b57c3
marhong2 <67293152+marhong2@users.noreply.github.com>
2021-11-08 15:53:51 -0800

    Lido app/marcus/xlookup (#21)
    * Added tests to Xlookup
    
    * Updated XLOOKUP
    
    * LIDO-2056: FILTER
    
    * LIDO-2056: FILTER
    
    * Ran yarn format
    
    * LIDO-1747 : XLOOKUP
    
    * LIDO-1747 : XLOOKUP
    
    * LIDO-1747 : XLOOKUP
    
    * yarn format
    
    * attempt2
    
    * LIDO-2056 : FILTER
    
    * LIDO-2056 : FILTER
    
    * LIDO-1747 : XLOOKUP
    
    * LIDO-1747 : XLOOKUP
    
    * LIDO-1747 : XLOOKUP
    
    * LIDO-1747 : XLOOKUP
    
    Co-authored-by: Tyler Adams <tyler@trylido.com>

--- formulas/functions/reference.js
+++ formulas/functions/reference.js
@@ -372,6 +372,200 @@ const ReferenceFunctions = {
       return tableArray[index][colIndexNum - 1];
     }
   },
+  /***
+   * @param lookup_value: the value we are looking for in our lookup_array
+   * @param lookup_array: the array were we search for lookup_value
+   * @param return_array: the array where our return value is
+   * @param if_not_found: OPTIONAL default value if lookup_value is not found
+   * @param match_mode: OPTIONAL:
+   *                         0: If none found, return #N/A. This is the default
+   *                         -1 - Exact match. If none found, return the next smaller item.
+   *                          1 - Exact match. If none found, return the next larger item.
+   *                          2 - A wildcard match where *, ?, and ~ have special meaning.
+   * @param search_mode: OPTIONAL:
+   *                          1 - Perform a search starting at the first item. This is the default.
+   *                         -1 - Perform a reverse search starting at the last item.
+   *                          2 - Perform a binary search that relies on lookup_array being sorted in ascending order. If not sorted, invalid results will be returned.
+   *                         -2 - Perform a binary search that relies on lookup_array being sorted in descending order. If not sorted, invalid results will be returned.
+   * Microsoft Link: https://support.microsoft.com/en-us/office/xlookup-function-b7fd680e-6d10-43e6-84f9-88eae8bf5929
+   ***/
+  XLOOKUP: (
+    lookup_value,
+    lookup_array,
+    return_array,
+    if_not_found = null,
+    match_mode = 0,
+    search_mode = 1
+  ) => {
+    try {
+      lookup_array = H.accept(lookup_array, Types.ARRAY);
+      return_array = H.accept(return_array, Types.ARRAY);
+    } catch (e) {
+      if (e instanceof FormulaError) {
+        throw FormulaError.NA;
+      }
+      throw e;
+    }
+    if (lookup_array.length != return_array.length) {
+      throw FormulaError.NA;
+    }
+    if (Array.isArray(lookup_array[0])) {
+      throw FormulaError.VALUE;
+    }
+    if (Array.isArray(return_array[0])) {
+      throw FormulaError.VALUE;
+    }
+
+    lookup_value = H.accept(lookup_value);
+    search_mode = H.accept(search_mode, Types.NUMBER);
+    match_mode = H.accept(match_mode, Types.NUMBER);
+    if (![1, -1, 2, -2].includes(search_mode)) {
+      throw FormulaError.VALUE;
+    }
+    if (![0, 1, -1, 2].includes(match_mode)) {
+      throw FormulaError.VALUE;
+    }
+    if (match_mode === 2 && [2, -2].includes(search_mode)) {
+      throw FormulaError.VALUE;
+    }
+    if (if_not_found != null) {
+      if (if_not_found.omitted) {
+        if_not_found = null;
+      } else {
+        if_not_found = H.accept(
+          if_not_found,
+          null,
+          null,
+          true,
+          false,
+          (test = true)
+        );
+      }
+    }
+    //If search mode is 1 or -1, then we run a linear search on the input arrays
+    if ([1, -1].includes(search_mode)) {
+      //Transform is 0 if search mode is 1 (we want to go through the array in order)
+      //Transform is the last index if search_mode is -1 (we go through in reverse order)
+      const transform = search_mode === 1 ? 0 : lookup_array.length - 1;
+      var minDiff = { index: -1, value: Number.MAX_VALUE };
+      var maxDiff = { index: -1, value: Number.MAX_VALUE };
+
+      for (var i = 0; i < lookup_array.length; i++) {
+        const currIndex = Math.abs(transform - i);
+        const currValue = H.accept(lookup_array[currIndex]);
+        const comparison = H.XLOOKUP_HELPER(
+          lookup_value,
+          currValue,
+          match_mode != 2
+        );
+
+        if (comparison === 0) {
+          return return_array[currIndex];
+        }
+        if ([1, -1].includes(match_mode)) {
+          if (comparison < 0 && Math.abs(comparison) < minDiff.value) {
+            minDiff.index = currIndex;
+            minDiff.value = Math.abs(comparison);
+          }
+          if (comparison > 0 && comparison < maxDiff.value) {
+            maxDiff.index = currIndex;
+            maxDiff.value = comparison;
+          }
+        }
+      }
+      if (minDiff.index >= 0 && match_mode === -1) {
+        return return_array[minDiff.index];
+      }
+      if (maxDiff.index >= 0 && match_mode === 1) {
+        return return_array[maxDiff.index];
+      }
+      if (if_not_found != null) {
+        return if_not_found;
+      }
+      throw FormulaError.NA;
+
+      //In the case where search mode is 2 or -2, we run a binary search
+    } else if ([2, -2].includes(search_mode)) {
+      var front = 0;
+      var back = lookup_array.length - 1;
+      while (front < back - 1) {
+        const middle = Math.floor((front + back) / 2);
+        const currValue = H.accept(lookup_array[middle]);
+        const comparison = H.XLOOKUP_HELPER(
+          lookup_value,
+          currValue,
+          match_mode != 2,
+          [2, -2].includes(search_mode)
+        );
+        if (comparison === 0) {
+          return return_array[middle];
+        } else if (comparison < 0 && search_mode === 2) {
+          front = middle;
+        } else if (comparison < 0 && search_mode === -2) {
+          back = middle;
+        } else if (comparison > 0 && search_mode === 2) {
+          back = middle;
+        } else if (comparison > 0 && search_mode === -2) {
+          front = middle;
+        }else {
+          throw "Unreachable Code Error"
+        }
+      }
+      var comparisonFront = H.XLOOKUP_HELPER(
+        lookup_value,
+        lookup_array[front],
+        match_mode != 2
+      );
+      var comparisonBack = H.XLOOKUP_HELPER(
+        lookup_value,
+        lookup_array[back],
+        match_mode != 2
+      );
+
+      if (comparisonFront === 0) {
+        return return_array[front];
+      }
+      if (comparisonBack === 0) {
+        return return_array[back];
+      }
+      //If search mode === 2 search_mode === 1, we have to check front first, 
+      //b/c its in ascending order and we want the next largest item
+      if (comparisonFront > 0 && match_mode === 1 && search_mode === 2) {
+        return return_array[front];
+      }
+      if (comparisonBack > 0 && match_mode === 1 && search_mode === 2) {
+        return return_array[back];
+      }
+      //If search mode === 2 search_mode === -1, we have to check back first, 
+      //b/c its in ascending order and we want the next smaller item
+      if (comparisonBack < 0 && match_mode === -1 && search_mode === 2) {
+        return return_array[back];
+      }
+      if (comparisonFront < 0 && match_mode === -1 && search_mode === 2) {
+        return return_array[front];
+      }
+      //If search mode === -2 search_mode === 1, we have to check back first, 
+      //b/c its in descending order and we want the next largest item
+      if (comparisonBack > 0 && match_mode === 1 && search_mode === -2) {
+        return return_array[back];
+      }
+      if (comparisonFront > 0 && match_mode === 1 && search_mode === -2) {
+        return return_array[front];
+      }
+      //If search mode === -2 search_mode === -1, we have to check front first, 
+      //b/c its in descending order and we want the next smaller item
+      if (comparisonFront < 0 && match_mode === -1 && search_mode === -2) {
+        return return_array[front];
+      }
+      if (comparisonBack < 0 && match_mode === -1 && search_mode === -2) {
+        return return_array[back];
+      }
+      if (if_not_found) {
+        return if_not_found;
+      }
+    }
+    throw FormulaError.NA;
+  },
 };
 
 module.exports = ReferenceFunctions;
